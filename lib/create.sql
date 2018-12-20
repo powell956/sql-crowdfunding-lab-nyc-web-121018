fd19a7b148840dc537f93eabbb706b0ecd0a2fd0
powell956 <powell956@gmail.com>
2018-12-20 07:57:21 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,14 @@
-CREATE TABLE prject (
+CREATE TABLE project (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
 );
