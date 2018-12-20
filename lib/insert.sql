5099310cb64a19af28ca4e00abbe6bb2dffcce47
powell956 <powell956@gmail.com>
2018-12-20 08:11:21 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("WizzBang", "Toys", 20000, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Grincher", "Kitchen", 400000, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Shitstorm", "Games", 10000, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Pupperizer", "Pets", 30000, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("De-composter", "Tools", 550000, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Pay-per-News", "Software", 1000, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("The Jets", "Entertainment", 250000, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Zuit Crew", "Music", 2500, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("The Artist, Alone with a Wolf", "Arts", 500, "2018-12-19", "2019-01-02");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Creeper Collective", "Film", 1250000, "2018-12-19", "2019-01-02");
+
+INSERT INTO users (name, age) VALUES ("Dean Dorcus", 27);
