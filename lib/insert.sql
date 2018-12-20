a1fb1b06026c586d0918655922bbc638f533ba60
powell956 <powell956@gmail.com>
2018-12-20 08:18:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,3 +20,22 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("Creeper Collective", "Film", 1250000, "2018-12-19", "2019-01-02");
 
 INSERT INTO users (name, age) VALUES ("Dean Dorcus", 27);
+INSERT INTO users (name, age) VALUES ("Dick Tracey", 89);
+INSERT INTO users (name, age) VALUES ("Dan P. Owl", 28);
+INSERT INTO users (name, age) VALUES ("Arnold Layne", 22);
+INSERT INTO users (name, age) VALUES ("Emily Plays", 14);
+INSERT INTO users (name, age) VALUES ("Mister Mustard", 55);
+INSERT INTO users (name, age) VALUES ("Polythene Pam", 42);
+INSERT INTO users (name, age) VALUES ("Her Majesty", 90);
+INSERT INTO users (name, age) VALUES ("Pink Floyd", 25);
+INSERT INTO users (name, age) VALUES ("Michelle MyBelle", 33);
+INSERT INTO users (name, age) VALUES ("Lovely Rita", 34);
+INSERT INTO users (name, age) VALUES ("Helter Skelter", 27);
+INSERT INTO users (name, age) VALUES ("Mary Jane", 42);
+INSERT INTO users (name, age) VALUES ("Douglas Adams", 68);
+INSERT INTO users (name, age) VALUES ("Aldous Huxley", 70);
+INSERT INTO users (name, age) VALUES ("George Orwell", 75);
+INSERT INTO users (name, age) VALUES ("Chinua Achebe", 44);
+INSERT INTO users (name, age) VALUES ("Idris Elba", 45);
+INSERT INTO users (name, age) VALUES ("John Boyega", 27);
+INSERT INTO users (name, age) VALUES ("Tilda Swinton", 52);
